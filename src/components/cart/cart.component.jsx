930efc4b04647b79cfa77273d42ef1775a0e64cd
Yazan <yazankhirfan@gmail.com>
2020-04-21 12:50:53 -0400

    created cart-item component, connected cart dropdown with our cartItem reducer and fixed the quantity in cart utils

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,16 +1,27 @@
 import React from "react"; 
+import {connect} from "react-redux";
 import CustomButton from "../custom-button/custom-button.component"; 
 import "./cart.style.scss";
+import CartItem from "../cart-item/cart-item.component";
+
 
 //stateless compnentt
-const CartDropdown = () =>(
+const CartDropdown = ({cartItems}) =>(
 
     <div className="cart-dropdown">
         <div className="cart-items"></div>
+        {
+            cartItems.map(cartItem=> (
+            <CartItem key={cartItem.id} item={cartItem} /> 
+            ))}
         <CustomButton>Go To Checkout</CustomButton>
+
     </div>
 ); 
 
+const mapStateToProps = ({cart : {cartItems}}) =>({
+    cartItems
+})
 
 
-export default CartDropdown;
+export default connect (mapStateToProps)(CartDropdown);
