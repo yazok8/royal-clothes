1ab10fe7dd0d108d00b76a95762b290fe041bfc8
Yazan <yazankhirfan@gmail.com>
2020-05-03 12:37:13 -0400

    fixed the cart and checkout page styles

--- client/src/components/cart/cart.component.jsx
+++ client/src/components/cart/cart.component.jsx
@@ -3,28 +3,29 @@ import {connect} from "react-redux";
 import {createStructuredSelector} from "reselect";
 import {withRouter} from "react-router-dom";
 import CustomButton from "../custom-button/custom-button.component"; 
-import "./cart.style.scss";
 import CartItem from "../cart-item/cart-item.component";
 import {selectCartItems} from "../../redux/cart/cart.selectors";
 import {ToggleCartHidden} from "../../redux/cart/cart.action";
+import {CartDropdownContainer,EmptyMessageContainer,CartItemsContainer} from "./cart.styles"
 
 //stateless compnentt
 const CartDropdown = ({cartItems, history, dispatch}) =>(
 
-    <div className="cart-dropdown">
-        <div className="cart-items"></div>
+    <CartDropdownContainer>
+        <CartItemsContainer>
         {
             cartItems.length ? (
             cartItems.map(cartItem=> (
             <CartItem key={cartItem.id} item={cartItem} /> 
             ))
-            ):(<span className="empty-message"> your cart is empty</span>)
+            ):(<EmptyMessageContainer> your cart is empty</EmptyMessageContainer>)
 
         }
-        <CustomButton 
-        onClick={()=>{ history.push("/checkout");dispatch(ToggleCartHidden())}}>Go To Checkout</CustomButton>
+        </CartItemsContainer>
+        <CustomButton inverted={true} 
+        onClick={()=>{ history.push("/checkout");dispatch(ToggleCartHidden())}}>Checkout</CustomButton>
 
-    </div>
+    </CartDropdownContainer>
 ); 
 //this will make that the cart dropdown is not being rendered whenver the state changes unrelated to the cart items. 
 const mapStateToProps = createStructuredSelector({
