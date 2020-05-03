81c98b07608a21b1e7065d661d037c610a48cf73
Yazan <yazankhirfan@gmail.com>
2020-05-02 23:17:09 -0400

    updated .gitignore files and fixed error in server.js file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express= require("express"); 
-const bodyParser= require("body-parser"); 
 const cors= require("cors"); 
+const bodyParser= require("body-parser"); 
 const path= require("path"); 
 
 if(process.env.NODE_ENV!=="production") require("dotenv").config(); 
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "clien/build")));
+    app.use(express.static(path.join(__dirname, "client/build")));
 
     app.get("*", (req, res)=>{
         res.sendFile(path.join(__dirname, "client/build", "index.html"))
