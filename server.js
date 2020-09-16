5aa93875208149ba5ba0d2657795433893b7f50a
Yazan <yazankhirfan@gmail.com>
2020-09-16 10:53:47 -0400

    removed heroku suggestion of how to fix sigterm error as it didn't work

--- server.js
+++ server.js
@@ -35,29 +35,6 @@ app.listen(port, (error) => {
   console.log('server running on port ' + port);
 });
 
-const http = require('http');
-
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'));
-
-setInterval(console.log.bind(console, 'tick'), 1000);
-http
-  .createServer((req, res) => res.end('hi'))
-  .listen(process.env.PORT || 3000, () => console.log('Listening'));
-
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${signal}...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log('...waited 5s, exiting.');
-      process.exit(err ? 1 : 0);
-    }, 5000).unref();
-  };
-}
-
 app.get('/service-worker.js', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
 });
