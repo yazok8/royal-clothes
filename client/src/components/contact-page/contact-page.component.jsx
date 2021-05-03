705dd7ba615b5c7398f74ea68cfcaf1528569387
yazan <yazankhirfan@gmail.com>
2021-05-03 16:52:12 -0400

    updated reopsitory dependencies

--- client/src/components/contact-page/contact-page.component.jsx
+++ client/src/components/contact-page/contact-page.component.jsx
@@ -1,86 +1,80 @@
-import React, {useState} from "react";
-import FormInput from "../form-input/form-input.component";
-import CustomButton from"../custom-button/custom-button.component"; 
-import axios from "axios";
-import {ContactUsContainer, ContactPageTitle} from "./contact-page.styles"
+import React, { useState } from 'react';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+import axios from 'axios';
+import { ContactUsContainer, ContactPageTitle } from './contact-page.styles';
 
 const SendEmail = () => {
- 
-    const [userDetails, setUserDetails]= useState({
-      name:"",
-      email:"",
-      message:"",
-  
-      
+  const [userDetails, setUserDetails] = useState({
+    name: '',
+    email: '',
+    message: '',
   });
-  
-  const {name, email, message}=userDetails;
-  const handleSubmit = event => {
-  
+
+  const { name, email, message } = userDetails;
+  const handleSubmit = (event) => {
     event.preventDefault();
-    
-      axios.post("/contact",{
-        name: userDetails.name, 
-        email: userDetails.email, 
-        message: userDetails.message
-        })
-    .then(function (response) {
-      console.log(response)
-  })
-  .catch(function (error) {
-      console.log(error)
-  }) 
 
-  setUserDetails({
-      name:"", 
-      email:"", 
-      message:""
-  })
- 
-  }
-  
-  
-  const handleChange = event=>{
-  
-    const{name, value }=event.target
-  
-  setUserDetails({...userDetails,[name]: value});
-  
-  }
-  
-  
-    return (
-        <ContactUsContainer>
-            <ContactPageTitle>Contact Us</ContactPageTitle>
+    axios
+      .post('/contact', {
+        name: userDetails.name,
+        email: userDetails.email,
+        message: userDetails.message,
+      })
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+    setUserDetails({
+      name: '',
+      email: '',
+      message: '',
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setUserDetails({ ...userDetails, [name]: value });
+  };
+
+  return (
+    <ContactUsContainer>
+      <ContactPageTitle>Contact Us</ContactPageTitle>
       <form onSubmit={handleSubmit}>
-      <FormInput 
-            type="text"
-            name="name"
-            value={name}
-            onChange={handleChange}
-            label="name" 
-            required
-            />
-                 <FormInput 
-            type="email"
-            name="email"
-            value={email}
-            onChange={handleChange}
-            label="Email" 
-            required
-            />
-                 <FormInput 
-            type="textarea"
-            name="message"
-            value={message}
-            onChange={handleChange}
-            label="message" 
-            required
-            />
-     <CustomButton>Submit</CustomButton>
+        <FormInput
+          type="text"
+          name="name"
+          value={name}
+          onChange={handleChange}
+          label="name"
+          required
+        />
+        <FormInput
+          type="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+          label="Email"
+          required
+        />
+        <FormInput
+          type="textarea"
+          name="message"
+          rows="2"
+          cols="50"
+          value={message}
+          onChange={handleChange}
+          label="message"
+          required
+        />
+        <CustomButton>Submit</CustomButton>
       </form>
-      </ContactUsContainer>
-    );
-    }
+    </ContactUsContainer>
+  );
+};
 
-    export default SendEmail;
+export default SendEmail;
