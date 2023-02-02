1762da9c0d5f137063c032a3a722a6f508b36b5e
yazok8 <yazankhirfan@gmail.com>
2023-02-01 22:08:13 -0500

    move firebase config to a seperate file

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,17 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firebase-firestore';
 import 'firebase/auth';
+import config from "./config"
 
-const config= {
-    apiKey: "AIzaSyBgyJRTH8FpKAK41m8ooxf9SlFx4atrSAQ",
-    authDomain: "royl-db.firebaseapp.com",
-    databaseURL: "https://royl-db.firebaseio.com",
-    projectId: "royl-db",
-    storageBucket: "royl-db.appspot.com",
-    messagingSenderId: "1090387736595",
-    appId: "1:1090387736595:web:bb74bee9cf41254c7c67e6",
-    measurementId: "G-J91RDBWX33"
-  };
 
   firebase.initializeApp(config);
 
